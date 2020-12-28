81c5dc5f8b98ac836a882b3c87a58618528bd939
EmersonBraun <emersonfbraun@gmail.com>
2020-12-28 08:57:15 -0300

    feat: add rate limit

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import * as rateLimit from 'express-rate-limit';
 import helmet from 'helmet';
 import { AppModule } from './app.module';
 import { PrettyLogger } from './common/logger/pretty-log';
@@ -7,14 +9,19 @@ import { setupDocumentation } from './config/documentation';
 
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true, logger: console });
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, { cors: true, logger: console });
 
-  app.use(helmet())
-  app.useLogger(new PrettyLogger);
-  app.useGlobalPipes(new ValidationPipe({
-    transform: true,
-  }));
-  void setupDocumentation(app)
+  app.use(helmet()) // protection
+  app.use(
+    rateLimit({
+      windowMs: 15 * 60 * 1000, // 15 minutes
+      max: 100, // limit each IP to 100 requests per windowMs
+    }),
+  )
+  app.useLogger(new PrettyLogger); // custom logger 
+  app.useGlobalPipes(new ValidationPipe({ transform: true,})); // class-validator
+  app.set('trust proxy', 1) // if used reverse-proxy
+  void setupDocumentation(app) // swagger documentations
 
   await app.listen(process.env.APP_PORT);
   console.log(`🚀 ${String(process.env.APP_NAME)} (API) is running on: http://localhost:${process.env.APP_PORT}`);
