04fc46075f001e50233680398ba577bbf97d6f85
EmersonBraun <emersonfbraun@gmail.com>
2020-12-28 18:41:17 -0300

    feat: remove unused functions

--- src/modules/Tasks/controller/task.controller.ts
+++ src/modules/Tasks/controller/task.controller.ts
@@ -1,18 +1,15 @@
 import {
   Body,
   Controller,
-  Delete,
   Get,
   HttpCode,
-  Param,
-  Post,
-  Put
+  Post
 } from '@nestjs/common';
 import {
   ApiBadRequestResponse,
   ApiCreatedResponse,
-  ApiNoContentResponse,
-  ApiNotFoundResponse,
+
+
   ApiOkResponse,
   ApiOperation,
   ApiTags
@@ -44,44 +41,5 @@ export class TasksController {
   @ApiBadRequestResponse({ type: ErrorResponse, description: 'Bad Request', })
   async search(@Body() data: CreateTasksDto): Promise<Tasks[]> {
     return await this.service.search(data);
-  }
-
-  @Post()
-  @HttpCode(201)
-  @ApiOperation({ summary: 'Create a new Tasks' })
-  @ApiCreatedResponse({ type: UpdateTasksDto, description: 'Created Tasks' })
-  @ApiBadRequestResponse({ type: ErrorResponse, description: 'Bad Request', })
-  async create(@Body() data: CreateTasksDto) {
-    return await this.service.create(data);
-  }
-
-  @Get(':id')
-  @HttpCode(200)
-  @ApiOperation({ summary: 'Search a Tasks by id' })
-  @ApiOkResponse({ type: UpdateTasksDto, description: 'The found Tasks' })
-  @ApiNotFoundResponse({ type: ErrorResponse, description: 'Not Found' })
-  async findById(@Param('id') id: string): Promise<Tasks> {
-    return await this.service.findById(id);
-  }
-
-  @Put(':id')
-  @HttpCode(200)
-  @ApiOperation({ summary: 'Update a Tasks' })
-  @ApiOkResponse({ type: UpdateTasksDto, description: 'Updated Tasks' })
-  @ApiNotFoundResponse({ type: ErrorResponse, description: 'Not Found' })
-  async update(
-    @Param('id') id: string,
-    @Body() data: UpdateTasksDto,
-  ): Promise<Tasks> {
-    return this.service.update(id, data);
-  }
-
-  @Delete(':id')
-  @HttpCode(204)
-  @ApiOperation({ summary: 'Delete a Tasks' })
-  @ApiNoContentResponse({ description: 'Deleted Tasks' })
-  @ApiNotFoundResponse({ type: ErrorResponse, description: 'Not Found' })
-  async delete(@Param('id') id: string): Promise<void> {
-    await this.service.delete(id);
   }
 }
