6ad0377d0a339b0576eb7b00e4aeb4b0869babaf
EmersonBraun <emersonfbraun@gmail.com>
2021-02-15 17:56:42 -0300

    refactor: andamentocoger entity

--- src/modules/andamento/entity/andamento.entity.ts
+++ src/modules/andamento/entity/andamento.entity.ts
@@ -2,12 +2,7 @@ import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity('andamentos')
 export class Andamento {
-  @PrimaryGeneratedColumn()
-  id: number;
-
-  @Column({ nullable: true })
-  andamento: string;
-
-  @Column({ nullable: true })
-  procedimento: string;
+  @PrimaryGeneratedColumn() id: number;
+  @Column({ nullable: true }) andamento: string;
+  @Column({ nullable: true }) procedimento: string;
 }
