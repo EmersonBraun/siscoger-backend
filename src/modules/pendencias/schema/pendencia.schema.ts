ac3ed0062ef51eae74c7c7d1318f02461b5fbd94
EmersonBraun <emersonfbraun@gmail.com>
2021-02-16 10:22:31 -0300

    refactor: pendencia entity

--- src/modules/pendencias/schema/pendencia.schema.ts
+++ src/modules/pendencias/schema/pendencia.schema.ts
@@ -5,18 +5,13 @@ import { Any } from 'typeorm';
 @Schema({ timestamps: true })
 export class Pendencia {
   @Prop() cdopm: string;
-
   @Prop() id_proc: number;
-
   @Prop() sjd_ref: number;
-
   @Prop() sjd_ref_ano: number;
-
   @Prop() proc: string;
-
   @Prop([String]) pendencias: string[];
-
   @Prop([Any]) state: any[];
 }
+
 export type PendenciaDocument = Pendencia & Document;
 export const PendenciaSchema = SchemaFactory.createForClass(Pendencia);
