343369e2d9662cb634050c71b6545cbfbde30e9b
EmersonBraun <emersonfbraun@gmail.com>
2021-02-16 10:12:57 -0300

    refactor: feriado entity

--- src/modules/feriado/entity/feriado.entity.ts
+++ src/modules/feriado/entity/feriado.entity.ts
@@ -2,12 +2,7 @@ import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity('feriados')
 export class Feriado {
-  @PrimaryGeneratedColumn()
-  id: number;
-
-  @Column({ type: 'date', nullable: true })
-  data: Date;
-
-  @Column({ nullable: true })
-  feriado: string;
+  @PrimaryGeneratedColumn() id: number;
+  @Column({ type: 'date', nullable: true }) data: Date;
+  @Column({ nullable: true }) feriado: string;
 }
