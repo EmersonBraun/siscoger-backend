ab3fe9bb680b6e5e66a13b2417e5615fda464365
EmersonBraun <emersonfbraun@gmail.com>
2021-02-16 10:13:56 -0300

    refactor: gradacao entity

--- src/modules/gradacao/entity/gradacao.entity.ts
+++ src/modules/gradacao/entity/gradacao.entity.ts
@@ -2,12 +2,7 @@ import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity('gradacoes')
 export class Gradacao {
-  @PrimaryGeneratedColumn()
-  id: number;
-
-  @Column({ nullable: true })
-  gradacao: string;
-
-  @Column({ nullable: true })
-  rel: string;
+  @PrimaryGeneratedColumn() id: number;
+  @Column({ nullable: true }) gradacao: string;
+  @Column({ nullable: true }) rel: string;
 }
