265316994866d6076bdb61cd1e1c98103ae742e0
EmersonBraun <emersonfbraun@gmail.com>
2021-02-15 21:53:22 -0300

    feat: update readme

--- src/modules/andamentocoger/entity/andamentocoger.entity.ts
+++ src/modules/andamentocoger/entity/andamentocoger.entity.ts
@@ -2,12 +2,7 @@ import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity('andamentoscoger')
 export class Andamentocoger {
-  @PrimaryGeneratedColumn()
-  id: number;
-
-  @Column({ nullable: true })
-  andamentocoger: string;
-
-  @Column({ nullable: true })
-  procedimento: string;
+  @PrimaryGeneratedColumn() id: number;
+  @Column({ nullable: true }) andamentocoger: string;
+  @Column({ nullable: true }) procedimento: string;
 }
