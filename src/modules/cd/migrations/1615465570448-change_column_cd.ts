875d184ab8f156c5061374d726fa24094aae7240
EmersonBraun <emersonfbraun@gmail.com>
2021-03-11 11:11:03 -0300

    fix: solve cd and cj migrations

--- src/modules/cd/migrations/1615465570448-change_column_cd.ts
+++ src/modules/cd/migrations/1615465570448-change_column_cd.ts
@@ -2,12 +2,12 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class changeColumnCd1615465570448 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.renameColumn('cd', 'exclusao_txt', 'exclusao_text');
+    await queryRunner.renameColumn('cd', 'exclusao_text', 'exclusao_txt');
     await queryRunner.renameColumn('cd', 'sintese_text', 'sintese_txt');
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.renameColumn('cd', 'exclusao_text', 'exclusao_txt');
+    await queryRunner.renameColumn('cd', 'exclusao_txt', 'exclusao_text');
     await queryRunner.renameColumn('cd', 'sintese_txt', 'sintese_text');
   }
 }
--- src/modules/cj/migrations/1615462226297-change_column_cj.ts
+++ src/modules/cj/migrations/1615462226297-change_column_cj.ts
@@ -2,12 +2,12 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class changeColumnCj1615462226297 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.renameColumn('cj', 'exclusao_txt', 'exclusao_text');
+    await queryRunner.renameColumn('cj', 'exclusao_text', 'exclusao_txt');
     await queryRunner.renameColumn('cj', 'sintese_text', 'sintese_txt');
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.renameColumn('cj', 'exclusao_text', 'exclusao_txt');
+    await queryRunner.renameColumn('cj', 'exclusao_txt', 'exclusao_text');
     await queryRunner.renameColumn('cj', 'sintese_txt', 'sintese_text');
   }
 }
